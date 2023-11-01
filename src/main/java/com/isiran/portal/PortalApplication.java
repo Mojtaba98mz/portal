fd3b28c0ab4ac90fde019660a1dab3fd4f463373
mojtaba <mojtaba98.mz@gmail.com>
2023-11-01 08:47:38 +0330

    add jasypt for properties encryption

--- src/main/java/com/isiran/portal/PortalApplication.java
+++ src/main/java/com/isiran/portal/PortalApplication.java
@@ -1,12 +1,14 @@
 package com.isiran.portal;
 
 import com.isiran.portal.config.PortalProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 @EnableConfigurationProperties({LiquibaseProperties.class, PortalProperties.class })
 public class PortalApplication {
 
