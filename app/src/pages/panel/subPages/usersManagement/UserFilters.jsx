e82fbca9723bedcaf6477e1a8408c10e8f35ff7e
internet <internet@localhost.localdomain>
2024-01-07 12:11:24 +0330

    install stylis-plugin-rtl and use in userFilters inputs

--- app/src/pages/panel/subPages/usersManagement/UserFilters.jsx
+++ app/src/pages/panel/subPages/usersManagement/UserFilters.jsx
@@ -1,9 +1,13 @@
 import { SearchSharp } from '@mui/icons-material'
 import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, TextField, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles';
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { clearUserSearch, saveUserData } from '../../../../reduxTK/features/searchUser/searchUserSlice'
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 const UserFilters = () => {
 
@@ -29,19 +33,26 @@ const UserFilters = () => {
     }
   }
 
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [rtlPlugin],
+  });
+
   return (
     <>
       <Accordion variant='elevation' sx={{ width: "100%", direction: "rtl", marginY: "15px" }}>
         <AccordionSummary><Typography variant='body1' fontFamily="Yekan">جستجوی کاربر</Typography></AccordionSummary>
         <AccordionDetails>
           <Box sx={{ marginLeft: "auto", marginY: "5px", display: "flex", justifyContent: "space-between" }}>
-            <TextField variant="outlined" size="small" label="نام" sx={{ direction: "ltr",maxWidth:"220px" }} onChange={(e) => setFirstName(e.target.value)} ></TextField>
-            <TextField variant="outlined" size="small" label="نام خانوادگی" sx={{ direction: "ltr",maxWidth:"220px" }} onChange={(e) => setLastName(e.target.value)} ></TextField>
-            <TextField variant="outlined" size="small" label="کدملی" sx={{ direction: "ltr",maxWidth:"220px" }} onChange={(e) => setNationalCode(e.target.value)} ></TextField>
+            <CacheProvider value={cacheRtl}>
+              <TextField variant="outlined" size="small" label="نام" InputLabelProps={{ shrink: true }} sx={{ direction: "ltr", maxWidth: "220px", textAlign: "right" }} onChange={(e) => setFirstName(e.target.value)} ></TextField>
+              <TextField variant="outlined" size="small" label="نام خانوادگی" InputLabelProps={{ shrink: true }} sx={{ direction: "ltr", maxWidth: "220px" }} onChange={(e) => setLastName(e.target.value)} ></TextField>
+              <TextField variant="outlined" size="small" label="کدملی" InputLabelProps={{ shrink: true }} sx={{ direction: "ltr", maxWidth: "220px" }} onChange={(e) => setNationalCode(e.target.value)} ></TextField>
+            </CacheProvider>
             <Box>
               <IconButton onClick={searchNationalCode}><SearchSharp /></IconButton>
-              <IconButton onClick={()=>dispatch(clearUserSearch())}><FilterAltOffIcon sx={{width:"17px"}} /></IconButton>
-          </Box>
+              <IconButton onClick={() => dispatch(clearUserSearch())}><FilterAltOffIcon sx={{ width: "17px" }} /></IconButton>
+            </Box>
           </Box>
         </AccordionDetails>
       </Accordion>
